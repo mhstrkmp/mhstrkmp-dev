f004d1f8b52a5bca7d3bf09b140de9ed9312759b
mhstrkmp <hello@mhstrkmp.dev>
2023-02-09 22:01:54 +0100

    chore: initial setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({});
+import partytown from '@astrojs/partytown'
+
+// https://astro.build/config
+import sitemap from '@astrojs/sitemap'
+
+// https://astro.build/config
+import tailwind from '@astrojs/tailwind'
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://mhstrkmp.dev',
+    integrations: [partytown(), sitemap(), tailwind()],
+})
---
+++ src/constants.ts
@@ -0,0 +1,13 @@
+// -------------------------------------
+// SITE CONFIG
+// -------------------------------------
+export const SITE_TITLE = 'MHSTRKMP.DEV'
+export const SITE_DESCRIPTION = 'Welcome to mhstrkmp.dev!'
+export const SITE_DEFAULT_LANG = 'en'
+
+// -------------------------------------
+// SOCIAL PROFILES
+// -------------------------------------
+export const SOCIAL_GITHUB = 'https://github.com/mhstrkmp'
+export const SOCIAL_LINKEDIN = 'https://www.linkedin.com/in/mhstrkmp/'
+export const SOCIAL_MASTODON = 'https://freiburg.social/@mhstrkmp'
